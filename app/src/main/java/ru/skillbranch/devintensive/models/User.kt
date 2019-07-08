34eebc5c05c76539bcc38d288fecaf996f044f70
Arthur Galiulin <Galiulin.Arthur@gmail.com>
2019-07-09 00:01:31 +0300

    solutions task_2

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,73 +1,55 @@
 package ru.skillbranch.devintensive.models
 
 import ru.skillbranch.devintensive.utils.Utils
-import java.util.Date;
+import java.util.Date
+import java.util.UUID
 
 data class User(
     val id: String,
     var firstName: String?,
     var lastName: String?,
     var avatar: String?,
-    var raiting: Int = 0,
+    var rating: Int = 0,
     var respect: Int = 0,
-    val lastVisit: Date? = null,
-    val isOnline: Boolean = false
+    var lastVisit: Date? = Date(),
+    var isOnline: Boolean = false
 ) {
 
-    var introBit :String
-
-    constructor(id: String, firstName: String?, lastName: String?) : this(
-        id = id,
-        firstName = firstName,
-        lastName = lastName,
-        avatar = null
-    )
+    constructor(id: String, firstName: String?, lastName: String?) :
+            this(id, firstName, lastName, null)
 
     constructor(id: String) : this(id, "John", "Doe")
 
-    init {
-        introBit = getIntro()
-        println("It's Alive!!!\n" +
-                "${if (lastName==="Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName!!!"}\n")
+    companion object Factory {
+        fun makeUser(fullName: String?): User {
+            if (fullName == null) {
+                return User(UUID.randomUUID().toString())
+            }
+            val (firstName, lastName) = Utils.parseFullName(fullName)
+            return User(UUID.randomUUID().toString(), firstName, lastName, null)
+        }
     }
 
-    private fun getIntro() ="""
-        tu tu ru tuuuu !!!
-        tu tu ru tuuuuuuuu ...
+    class Builder {
+        var id: String = ""
+        var firstName: String? = null
+        var lastName: String? = null
+        var avatar: String? = null
+        var rating: Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = Date()
+        var isOnline: Boolean = false
 
-        tu tu ru tuuuu !!!
-        tu tu ru tuuuuuuuu ...
-        ${"\n\n\n"}
-        $firstName $lastName
-
-
-    """.trimIndent()
-
-    fun printMe() = println(
-        """
-            id: $id
-            firstName: $firstName
-            lastName: $lastName
-            avatar: $avatar
-            raiting: $raiting
-            respect: $respect
-            lastVisit: $lastVisit
-            isOnline: $isOnline
-        """.trimIndent()
-    )
-
-    companion object Factory{
-        private var lastId : Int = -1
-        fun makeUser(fullName:String?):User{
-            lastId++
-
-
-
-            val (firstName, lastName) = Utils.parseFullName(fullName)
-
-            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String?) = apply { this.firstName = firstName }
+        fun lastName(lastName: String?) = apply { this.lastName = lastName }
+        fun avatar(avatar: String?) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+        fun build(): User {
+            return User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
         }
-
-
     }
 }
